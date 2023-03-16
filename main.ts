41f714632b3561d0f9589f88347677005ddc76e5
SeeYawn0 <123614122+SeeYawn0@users.noreply.github.com>
2023-03-15 20:20:07 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ input.onGesture(Gesture.FreeFall, function () {
         radio.sendString(GPS)
     }
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 let GPS = ""
 radio.setGroup(6)
 let lat = 52.1818424
@@ -10,3 +13,5 @@ let long = 0.1789449
 let treefallmsg = "Tree has fallen"
 GPS = "" + lat + long + treefallmsg
 basic.showString(GPS)
+basic.showIcon(IconNames.Yes)
+basic.showString("scanning trees...")
